378548ec594f36d3b016c34b543dcb94718ba585
sacred70 <sacred70@mail.ru>
2023-07-25 19:02:19 +0700

    deploy

--- goals/serializers.py
+++ goals/serializers.py
@@ -9,7 +9,7 @@ from django.db import transaction
 class BoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Board
-        read_only_fields = ('id', 'created', 'updated', 'is_deleted')
+        read_only_fields = ('id', 'created', 'updated', 'is_delete')
         fields = '__all__'
 
 
