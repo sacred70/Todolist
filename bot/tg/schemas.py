5ff67b44cffd3b952630bfd2c2e5360ae3fa110c
sacred70 <sacred70@mail.ru>
2023-08-02 15:11:54 +0700

    deploy

--- bot/tg/schemas.py
+++ bot/tg/schemas.py
@@ -3,32 +3,6 @@ from pydantic import BaseModel
 
 class Chat(BaseModel):
     id: int
-
-
-class Message(BaseModel):
-    chat: Chat
-    text: str | None = None
-
-
-class UpdateObj(BaseModel):
-    update_id: int
-    message: Message
-
-
-class GetUpdatesResponse(BaseModel):
-    ok: bool
-    result: list[UpdateObj]
-
-
-class SendMessageResponse(BaseModel):
-    ok: bool
-    result: Message
-
-"""from pydantic import BaseModel
-
-
-class Chat(BaseModel):
-    id: int
     username: str | None = None
 
 
@@ -51,4 +25,3 @@ class GetUpdatesResponse(BaseModel):
 class SendMessageResponse(BaseModel):
     ok: bool
     result: Message
-"""
